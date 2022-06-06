d6b27b9cb50b54c4fc39117cf2f40e5d30a1f966
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-06 11:03:23 +0300

    ES6 Classes commit

--- src/components/ES6/Classes.jsx
+++ src/components/ES6/Classes.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export default function Classes() {
-    class Car{
-        constructor(name){
-            this.brand = name
-        }
-
-        present(){
-            return 'I have a '+this.brand
-        }
+  class Car {
+    constructor(name) {
+      this.brand = name;
     }
 
-    const myCar = new Car('Ford')
-    // document.write(myCar.present)
-    
-    // class inheritance
-    class Model extends car {
-        constructor(name,mod) {
-            super(name)
-            this.model= mod
-        }
+    present() {
+      return "I have a " + this.brand;
     }
+  }
+
+  const myCar = new Car("Ford");
+  // document.write(myCar.present)
+
+  // class inheritance
+  class Model extends Car {
+    constructor(name, mod) {
+      super(name);
+      this.model = mod;
+    }
+    show() {
+      return this.present() + ", it is a " + this.model;
+    }
+  }
+
+  const myCarModel = new Model("Ford", "Mustang");
 
   return (
     <div>
-        <h1>ES6 Classes</h1>
-        <p>{myCar.present()}</p>
-
+      <h1>ES6 Classes</h1>
+      <p>{myCar.present()}</p>
+      <p>{myCarModel.show()}</p>
     </div>
-  )
+  );
 }
