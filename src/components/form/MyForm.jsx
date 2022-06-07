6ce3fd71841fe5ce5d352550e8556821daee8052
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-07 11:13:38 +0300

    form commit

--- src/components/form/MyForm.jsx
+++ src/components/form/MyForm.jsx
@@ -3,19 +3,24 @@ import { useState } from "react";
 
 export const MyForm = () => {
   const [name, setName] = useState("");
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered is: ${name}`);
+  };
+  console.log(name);
   return (
     <div>
-      <h1 onSubmit={handleSubmit}>Form</h1>
-      <form>
+      <h1 >Form</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">
           Enter your name:
           <input
-           type="text"
-           value={name}
-           onChange={(e)=>setName(e.target.value)}
-            />
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
+        <input type="submit" value="Submit name" />
       </form>
     </div>
   );
