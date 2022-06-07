b6d424e1240ef20d25d7888337b0562da179ef89
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-07 09:57:38 +0300

    operators commit

--- src/components/operators/Goal.jsx
+++ src/components/operators/Goal.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-export const MissedGoal = () => {
-    return <h4>Missed</h4>;
-};
-export const MadeGoal = () => {
-  return <h4>Goal!</h4>;
-};
-
-
 export const Goal = (isGoal) => {
+  const MissedGoal = () => {
+    return <h4>Missed</h4>;
+  };
+  const MadeGoal = () => {
+    return <h4>Goal!</h4>;
+  };
   const Goal = isGoal;
   return (
     <div>
       <h1>Operators</h1>
-      {Goal ? <MadeGoal /> : <MissedGoal />}
+      <>{Goal ? <MadeGoal /> : <MissedGoal />}</>
     </div>
   );
 };
