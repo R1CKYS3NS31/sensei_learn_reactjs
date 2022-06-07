97c6e2d7d91a40e32ee04f781dea8af706186643
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-07 09:34:51 +0300

    setState commit

--- src/components/class_component/Car.jsx
+++ src/components/class_component/Car.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 export class Car extends React.Component {
-    constructor({props}){
-        super(props)
-        // properties are put in this.state object
-        this.state = {
-            color:'red'
-        }
-    }
+  constructor({ props }) {
+    super(props);
+    // properties are put in this.state object
+    this.state = {
+      color: "red",
+    };
+  }
+  changeColor = () => {
+    this.setState({
+      color: "purple",
+    });
+  };
   render() {
     return (
       <div>
         <h1>Class Components</h1>
-        <p>Hi, I am a {this.state.color} {this.props.model} Car!</p>
+        <p>
+          Hi, I am a {this.state.color} {this.props.model} Car!
+        </p>
         {/* || */}
         <p>Hi, I am a {this.props.color} Car!</p>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
       </div>
     );
   }
