63045ca32fd67230fba44b133fbbd70a720bf2d0
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-07 10:53:56 +0300

    form commit

--- src/components/form/MyForm.jsx
+++ src/components/form/MyForm.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const MyForm = () => {
-
+  const [name, setName] = useState("");
+  
   return (
     <div>
-        <h1>Form</h1>
-        <form>
-            <label htmlFor="name">Enter your name:
-            <input type="text" />
-            </label>
-        </form>
+      <h1 onSubmit={handleSubmit}>Form</h1>
+      <form>
+        <label htmlFor="name">
+          Enter your name:
+          <input
+           type="text"
+           value={name}
+           onChange={(e)=>setName(e.target.value)}
+            />
+        </label>
+      </form>
     </div>
-  )
-}
+  );
+};
