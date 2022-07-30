1978bebb3dcadbf722237015ee7fea720b6759ea
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-30 21:53:15 +0300

    react router dom commit

--- src/components/react_router_dom/ReactRouterDom.jsx
+++ src/components/react_router_dom/ReactRouterDom.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Blog } from '../pages/Blog'
-import { Contact } from '../pages/Contact'
-import { Home } from '../pages/Home'
-import { Layout } from '../pages/Layout'
-import { NoPage } from '../pages/NoPage'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Blog } from "../pages/Blog";
+import { Contact } from "../pages/Contact";
+import { Home } from "../pages/Home";
+import { Layout } from "../pages/Layout";
+import { NoPage } from "../pages/NoPage";
 
 export const ReactRouterDom = () => {
   return (
     <BrowserRouter>
-    <Routes path='/'element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='blogs' element={<Blog/>}/>
-        <Route path='contact' element={<Contact/>}/>
-        <Route path='*'element={<NoPage/>}/>
-
-    </Routes>
-    
+      <Routes path="/" element={<Layout />}>
+        {" "}
+        // ricky has a bug here
+        <Route
+          index
+          element={
+            <>
+              <Layout />
+              <Home />
+            </>
+          }
+        />
+        <Route path="blogs" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
