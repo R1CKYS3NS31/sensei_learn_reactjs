c55a77f280d0fb21b46b762bdca1272ad2b967b7
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-01 20:40:47 +0300

    css.module.css commit

--- src/components/css/Css.jsx
+++ src/components/css/Css.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import styles from './my_style.module.css'
 
 export const Css = () => {
   // js object for styling
   const myStyle = {
-    padding: "50px",
+    padding: "10px",
     fontFamily: "Sans-Serif",
   };
   return (
     <>
-      <h1 style={{ color: "green", backgroundColor: "black" }}>Hello Style!</h1>
+      <h1 style={{ color: "rgb(0, 255, 10)", backgroundColor: "black" }}>Hello Style!</h1>
       <p style={myStyle}>Styling JS object</p>
+      <h3 className={styles.bigblue}>Css Module</h3>
     </>
   );
 };
