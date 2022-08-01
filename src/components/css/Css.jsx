02128f3d3c66fa141ad0247cc19e31a54b327343
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-01 20:30:47 +0300

    css js object commit

--- src/components/css/Css.jsx
+++ src/components/css/Css.jsx
@@ -3,12 +3,13 @@ import React from "react";
 export const Css = () => {
   // js object for styling
   const myStyle = {
-    padding: "10px",
+    padding: "50px",
     fontFamily: "Sans-Serif",
   };
   return (
     <>
       <h1 style={{ color: "green", backgroundColor: "black" }}>Hello Style!</h1>
+      <p style={myStyle}>Styling JS object</p>
     </>
   );
 };
