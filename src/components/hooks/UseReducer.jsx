91264c5adcd6d43c97112e0b32e7f6744edb3b18
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-03 13:13:34 +0300

    useReducer commit

--- src/components/hooks/UseReducer.jsx
+++ src/components/hooks/UseReducer.jsx
@@ -1,12 +1,51 @@
 // The useReducer Hook is similar to the useState Hook.
 // It allows for custom state logic.
 // If you find yourself keeping track of multiple pieces of state that rely on complex logic, useReducer may be useful.
-import React from 'react'
+import React from "react";
+import { useReducer } from "react";
+// The reducer function contains your custom state logic and the initialStatecan be a simple value but generally will contain an object.
+// The useReducer Hook returns the current stateand a dispatchmethod.
+
+const initialTodos = [
+  { id: 1, title: "Todo 1", complete: false },
+  { id: 2, title: "Todo 2", complete: false },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      });
+
+    default:
+      return state;
+  }
+};
 
 export const UseReducer = () => {
-    return (
-        <div>
-            UseReducer
-        </div>
-    )
-}
+  const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: "COMPLETE", id: todo.id });
+  };
+  return (
+    <>
+      {todos.map((todo) => {
+        <div key={todo.id}>
+          <label htmlFor="todo">
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleComplete(todo)}
+            />
+          </label>
+        </div>;
+      })}
+    </>
+  );
+};
