86a7bb39f7b4e7e999de1bb4685ada4942f23db5
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-03 14:40:55 +0300

    useCallback commit

--- src/components/hooks/useCallback/UseCallback.jsx
+++ src/components/hooks/useCallback/UseCallback.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useCallback } from "react";
+import { useState } from "react";
+import Todos from "./Todos";
 // This allows us to isolate resource intensive functions so that they will not automatically run on every render.
 // The useCallback Hook only runs when one of its dependencies update.
 
 export const UseCallback = () => {
-    
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New todo with useCallback"]);
+  }, [todos]);
   return (
-    <div>UseCallback</div>
-  )
-}
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count} <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
