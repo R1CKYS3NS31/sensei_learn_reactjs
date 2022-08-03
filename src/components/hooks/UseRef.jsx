e4cee70f172c7fe6fd3356662d066279e9953f2b
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-03 12:38:42 +0300

    useRef accessing DOM commit

--- src/components/hooks/UseRef.jsx
+++ src/components/hooks/UseRef.jsx
@@ -1,22 +1,34 @@
 // The useRef Hook allows you to persist values between renders.
 // It can be used to store a mutable value that does not cause a re-render when updated.
-import React from 'react'
-import { useEffect } from 'react'
-import { useRef } from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
 export const UseRef = () => {
-    const [inputValue, setInputValue] = useState('')
-    const count = useRef(0)
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
 
-    useEffect(() => {
-        count.current= count.current+1
-    })
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
 
-    return (
-        <>
-        <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
-        <h3>Render Count: {count.current}</h3>
-        </>
-    )
-}
+  // useRef to access DOM elements
+  const inputElement = useRef();
+  const focusInout = () => {
+    inputElement.current.focus();
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h3>Render Count: {count.current}</h3>
+      <input type="text" ref={inputElement}/>
+      <button onClick={focusInout}>Focus input</button>
+    </>
+  );
+};
